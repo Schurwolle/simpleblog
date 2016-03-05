ece61c22a1bb0bd67a0af76f4c24bb8c68a3fee5
Schurwolle <cartmen023@hotmail.com>
2016-03-05 18:42:25 +0100

    google ad client

---
+++ resources/views/googleAdClient.blade.php
@@ -0,0 +1,46 @@
+<div>
+    <div class="textwidget">
+        <script type="text/javascript">
+            google_ad_client = "ca-pub-7420562600953210";   
+
+            google_ad_slot = "0255048281";
+
+            google_ad_width = 160;
+
+            google_ad_height = 600;             
+        </script>
+
+        <script type="text/javascript" src="http://pagead2.googlesyndication.com/pagead/show_ads.js"></script>
+    </div>
+                                  
+    <div class="textwidget">
+        <script charset="utf-8" src="http://widgets.twimg.com/j/2/widget.js"></script>
+        <script>
+            new TWTR.Widget({
+            version: 2,
+            type: 'profile',
+            rpp: 2,
+            interval: 30000,
+            width: '190',
+            height: 300,
+            theme: {
+            shell: {
+            background: '#248a24',
+            color: '#ffffff'
+            },
+            tweets: {
+            background: '#fffcff',
+            color: '#000000',
+            links: '#0020c2'
+            }
+            },
+            features: {
+            scrollbar: false,
+            loop: false,
+            live: false,
+            behavior: 'all'
+            }
+            }).render().start();
+        </script>
+    </div>
+</div>
