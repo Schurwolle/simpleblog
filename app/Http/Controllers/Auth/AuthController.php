d4b6c7eea44de1e8ce1ae36bd3b8f8321ecb54ee
Schurwolle <cartmen023@hotmail.com>
2016-03-14 11:56:08 +0100

    unique username validation

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -53,8 +53,8 @@ class AuthController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => 'required|max:255',
-            'email' => 'required|email|max:255|unique:users',
+            'name' => 'required|max:255|unique:users,name',
+            'email' => 'required|email|max:255|unique:users,email',
             'password' => 'required|confirmed|min:6',
         ]);
     }
