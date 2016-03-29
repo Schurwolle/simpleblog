ae95ba394543a5a8c3800181de75afc1341dfa0e
Schurwolle <cartmen023@hotmail.com>
2016-03-29 18:50:40 +0200

    editing tags jQuery - update (not finished)

--- resources/views/tags.blade.php
+++ resources/views/tags.blade.php
@@ -20,26 +20,6 @@
 	    			{!!Form::close()!!}
 				</td>
 			</tr>
-			<tr class="update"><td>
-						{!!Form::open(['url' => 'tags/'.$tag->name])!!}
-						
-							{!!Form::text('name', $tag->name,['class' => 'form-control', 'id' => 'name'])!!}
-							
-					</td>
-					<td>
-							{!!Form::button('<i class="fa fa-plus"></i> Update',['class' => 'btn btn-default', 'type' => 'submit'])!!}
-							{!!Form::button('<i class="fa fa-remove"></i> Cancel',['class' => 'btn btn-warning', 'style' => 'width: 85px;'])!!}
-
-						{!!Form::close()!!}
-					</td>
-					<td>
-					{!!Form::open(['method' => 'DELETE', 'url' => 'tags/'.$tag->name ])!!}
-
-	      				{!!Form::button('<i class="fa fa-trash"></i> Delete', array('id' => 'delete', 'class' => 'btn btn-danger'))!!}
-
-	    			{!!Form::close()!!}
-				</td>
-			</tr>
 		@endforeach
 	@else
 		<h3>There are no tags at the moment.</h3>
@@ -52,18 +32,25 @@
 @include('ConfirmDelete')
 
 <script type="text/javascript">
-	$('.update').hide();
+
 	$('td').children('.btn-default').on('click',function(){
-		$('.update').hide();
-		$('.tag').show();
-		$(this).closest('.tag').hide();
-		$(this).closest('tr').next('tr').show();
-		$(this).closest('tr').next('tr').find('input').focus();
+		var tagname = $('.updating').find('input').val();
+		$('.updating').removeClass('updating').addClass('tag').html('<td><a href="/tags/'+ tagname +'"><button class="btn btn-default">'+tagname +'</button></a></td><td><button class="btn btn-default"><i class="fa fa-edit"></i> Edit</button></td><td><form action="tags/'+ tagname +'" method="DELETE"><button id="delete" class="btn btn-danger"><i class="fa fa-trash"></i> Delete </button></form></td>');
+
+		var txt = $(this).closest('.tag').find('.btn-default').html();
+		var tagname = txt.substring(0, txt.length-3);
+		$(this).closest('.tag').removeClass('tag').addClass('updating');
+		$('.updating').html('<td><form action="/tags/'+ tagname +'"method="POST"><input type="text" class="form-control" value='+ tagname +' autofocus></td><td><button type="submit" class="btn btn-default"><i class="fa fa-plus"></i> Update</button><button class="btn btn-warning"><i class="fa fa-remove"></i> Cancel</button></form></td><td><form action="tags/'+ tagname +'" method="DELETE"><button id="delete" class="btn btn-danger"><i class="fa fa-trash"></i> Delete </button></form></td>');
+
+		$('.btn-warning').on('click', function(){
+		$('.updating').removeClass('updating').addClass('tag');
+		$(this).closest('.tag').html('<td><a href="/tags/'+ tagname +'"><button class="btn btn-default">'+txt +'</button></a></td><td><button class="btn btn-default"><i class="fa fa-edit"></i> Edit</button></td><td><form action="tags/'+ tagname +'" method="DELETE"><button id="delete" class="btn btn-danger"><i class="fa fa-trash"></i> Delete </button></form></td>')
+		});
+		
 	});
 
 	$('.btn-warning').on('click', function(){
-		$(this).closest('.update').hide();
-		$(this).closest('tr').prev('tr').show();
+		$('.update').remove();	
 	});
 </script>
 
