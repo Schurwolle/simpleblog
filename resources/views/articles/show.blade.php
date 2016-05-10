5a229805c785f40ff0c477cf85d96458589a801b
Schurwolle <cartmen023@hotmail.com>
2016-05-10 16:18:50 +0200

    mark tags in article page

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -54,7 +54,7 @@
 	@unless ($article->tags->isEmpty())
 		<h5>Tags: 
 			@foreach($article->tags as $tag)
-				<a href="/tags/{{ $tag->name }}"><button class="btn btn-default btn-xs"> {{ $tag->name }} </button></a>
+				<a href="/tags/{{ $tag->name }}"><button class="btn btn-default btn-xs {{ isset($query_words) && $tag->name == strtolower(implode($query_words)) ? 'marker' : '' }}"> {{ $tag->name }} </button></a>
 			@endforeach
 		</h5>
 	@endunless
