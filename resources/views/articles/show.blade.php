3b4205d03fed3d2de4cfde9f3ba6704dff101587
Schurwolle <cartmen023@hotmail.com>
2016-06-09 11:38:54 +0200

    new comment - hide/show fix

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -222,11 +222,14 @@
 					} else {
 			        	$('#numComm').text(numComm + ' Comments:');
 			        }
-					hr.after('<div class="row comment" style="display:none;"><div class="col-sm-2"><div class="thumbnail"><a href="'+ href +'"><img src='+ src +'></a></div></div><div class="col-sm-10"><div class="panel panel-default"><div class="panel-heading comment-heading"><a class="black" href="'+ href +'"><strong>'+ username +'</strong></a><span class="text-muted"> commented 1 second ago</span></div><div class="panel-body" name="panelbody"></div><div id="'+ comment.id +'" class="panel-body"><table><tr><td><button id="edit" class="btn btn-primary"><i class="fa fa-edit"></i> Edit</button></td><td><button class="btn btn-danger" id ="deleteComment" data-token="{{ csrf_token() }}"><i class="fa fa-trash"></i> Delete</button></td></tr></table></div></div></div></div>');
+					hr.after('<div class="row comment" style="display:none;"><div class="col-sm-2"><div class="thumbnail"><a href="'+ href +'"><img src='+ src +'></a></div></div><div class="col-sm-10"><div class="panel panel-default"><div class="panel-heading comment-heading"><a class="black" href="'+ href +'"><strong>'+ username +'</strong></a><span class="text-muted"> commented 1 second ago</span></div><div class="panel-body new" name="panelbody"></div><div id="'+ comment.id +'" class="panel-body"><table><tr><td><button id="edit" class="btn btn-primary"><i class="fa fa-edit"></i> Edit</button></td><td><button class="btn btn-danger" id ="deleteComment" data-token="{{ csrf_token() }}"><i class="fa fa-trash"></i> Delete</button></td><td><button class="btn btn-info" id="hideComment">Hide Comment</button></td></tr></table></div></div></div></div>');
 					$('div#'+comment.id).prev('.panel-body').text(comment.body);
 					$('div#'+comment.id).parents('.row').slideDown();
 					$('button#edit').on('click', updating);
 					$('button#deleteComment').on('click', confirmDeleteComment);
+					$('button#hideComment').on('click', function() {
+						hideComment($(this));
+					});
 					$('textarea#add').val('');
 					$('textarea#add').height(80);
 					$('#counters')
