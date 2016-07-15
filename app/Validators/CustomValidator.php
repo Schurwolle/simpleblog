4dbddab612cab55a241d7d010899d332e33d2c27
Schurwolle <cartmen023@hotmail.com>
2016-07-15 14:27:36 +0200

    CKE imgs validation - fix

--- app/Validators/CustomValidator.php
+++ app/Validators/CustomValidator.php
@@ -21,7 +21,9 @@ class CustomValidator
             {                
                 $photo = substr($matches[0][$i], 9, strpos($matches[0][$i], '"', 9) - 9);
                 preg_match('#src="(.*?)"#', $matches[0][$i], $thumb);
-                if(!starts_with($photo, '/pictures/') && getimagesize($photo) == false || (!starts_with($thumb[1], '/pictures/') && getimagesize($thumb[1]) == false))
+                if(starts_with($photo, '/pictures/')) $photo = substr($photo, 1);
+                if(starts_with($thumb[1], '/pictures/')) $thumb[1] = substr($thumb[1], 1);
+                if((!starts_with($photo, 'pictures/') && (!filter_var($photo, FILTER_VALIDATE_URL))) || (!getimagesize($photo)) || ((!starts_with($thumb[1], 'pictures/')) && (!filter_var($thumb[1], FILTER_VALIDATE_URL))) || (!getimagesize($thumb[1])))
                 {
                     return false;
                 }
