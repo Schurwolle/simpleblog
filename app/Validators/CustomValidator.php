34673e1c0a6b5dd6afd17d5c86fc0d7aba060053
Schurwolle <cartmen023@hotmail.com>
2016-07-24 13:54:34 +0200

    CKE imgs server side validation - refactor & update

--- app/Validators/CustomValidator.php
+++ app/Validators/CustomValidator.php
@@ -21,11 +21,9 @@ class CustomValidator
             {                
                 $photo = substr($matches[0][$i], 9, strpos($matches[0][$i], '"', 9) - 9);
                 preg_match('#src="(.*?)"#', $matches[0][$i], $thumb);
-                if(starts_with($photo, '/pictures/')) $photo = substr($photo, 1);
-                if(starts_with($thumb[1], '/pictures/')) $thumb[1] = substr($thumb[1], 1);
-                if((!starts_with($photo, 'pictures/') && (!filter_var($photo, FILTER_VALIDATE_URL))) || (!getimagesize($photo)) || ((!starts_with($thumb[1], 'pictures/')) && (!filter_var($thumb[1], FILTER_VALIDATE_URL))) || (!getimagesize($thumb[1])))
+                if(starts_with($photo, '/pictures/') && (!file_exists(base_path().'/public'.$photo)) || starts_with($thumb[1], '/pictures/') && (!file_exists(base_path().'/public'.$thumb[1])) || ((!starts_with($photo, '/pictures/')) && ((!filter_var($photo, FILTER_VALIDATE_URL)) || (!getimagesize($photo)))) || ((!starts_with($thumb[1], '/pictures/')) && ((!filter_var($thumb[1], FILTER_VALIDATE_URL)) || (!getimagesize($thumb[1])))))
                 {
-                    return false;
+                	return false;
                 }
             }
         }
