8b6fbff32cb9375ef1cbc93f73b7db7dca107c1a
Schurwolle <cartmen023@hotmail.com>
2016-07-25 14:39:57 +0200

    CKE imgs server side validation - refactor

--- app/Validators/CustomValidator.php
+++ app/Validators/CustomValidator.php
@@ -21,12 +21,21 @@ class CustomValidator
             {                
                 $photo = substr($matches[0][$i], 9, strpos($matches[0][$i], '"', 9) - 9);
                 preg_match('#src="(.*?)"#', $matches[0][$i], $thumb);
-                if(starts_with($photo, '/pictures/') && (!file_exists(base_path().'/public'.$photo)) || starts_with($thumb[1], '/pictures/') && (!file_exists(base_path().'/public'.$thumb[1])) || ((!starts_with($photo, '/pictures/')) && ((!filter_var($photo, FILTER_VALIDATE_URL)) || (!getimagesize($photo)))) || ((!starts_with($thumb[1], '/pictures/')) && ((!filter_var($thumb[1], FILTER_VALIDATE_URL)) || (!getimagesize($thumb[1])))))
+                if(starts_with($photo, '/pictures/') && (!file_exists(base_path().'/public'.$photo)) || starts_with($thumb[1], '/pictures/') && (!file_exists(base_path().'/public'.$thumb[1])) || ((!starts_with($photo, '/pictures/')) && ((!$this->url_exists($photo)) || (!getimagesize($photo)))) || ((!starts_with($thumb[1], '/pictures/')) && ((!$this->url_exists($thumb[1])) || (!getimagesize($thumb[1])))))
                 {
                 	return false;
                 }
             }
         }
         return true;
+	}
+
+	private function url_exists($url)
+	{
+		$url_headers = @get_headers($url);
+		if($url_headers[0] == 'HTTP/1.1 404 Not Found') {
+		    return false;
+		}
+		return true;
 	}
 }
