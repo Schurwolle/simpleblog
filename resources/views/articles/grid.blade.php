83af8bc884036a2489ae50bd3ef6af683ca10e0c
Schurwolle <cartmen023@hotmail.com>
2016-10-12 13:57:28 +0200

    grid view - css update

--- resources/views/articles/grid.blade.php
+++ resources/views/articles/grid.blade.php
@@ -1,14 +1,18 @@
 @extends('layouts.app')
 
+@section('sides')
+	@include('leftandright')
+@endsection
+
 @section('content')
 	<table>
 	<tr>
 		@foreach($articles as $chunk)
 			<tr>
 				@foreach($chunk as $article)
-					<td>
+					<td style="padding: 10px;">
 						<h3 align="middle"><a class="black" href="/articles/{{ $article->slug }}/">{{$article->title}}</a></h1>
-						<a href="/articles/{{$article->slug}}">{{ Html::image(('pictures/'.$article->id.'thumbnail'), null) }}
+						<a href="/articles/{{$article->slug}}">{{ Html::image(('pictures/'.$article->id.'thumbnail'), null, ['class' => 'grid-image']) }}
 					</td>
 				@endforeach
 			</tr>
